c2b775a5d0953b8d33da5f5327d133e35e4218d9
wufubin <1227185979@qq.com>
2026-07-31 18:20:37 +0800

    修复：移除内部依赖，使用公网npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
-import { CodeInspectorPlugin } from "@rdservices/aime-code-inspector";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -9,9 +8,6 @@ export default defineConfig(({ mode }) => {
   return {
     base: "./",
     plugins: [
-      CodeInspectorPlugin({
-        bundler: "vite",
-      }),
       react(),
     ],
     resolve: {
